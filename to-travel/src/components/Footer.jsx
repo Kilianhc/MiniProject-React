ce484ef7651f2a2d11b2d70e859a625b7d4b7e7b
Kilian H <kilianhc@hotmail.com>
2025-02-13 11:02:29 +0000

    Finish version 1

--- to-travel/src/components/Footer.jsx
+++ to-travel/src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import {Link} from "react-router-dom"
+import GitIconFooter from "../assets/images/GitIconFooter.png"
 
 export default function Footer() {
     return (
         <div id="footer">
-            <a href="https://github.com/Kilianhc/MiniProject-React">Github.com/Kilianhc</a>
+            <img src={GitIconFooter} alt={GitIconFooter} />
+            <a target="_blank" href="https://github.com/Kilianhc/MiniProject-React">Github Repository</a>
         </div>
     )
 }
--- to-travel/src/pages/About.jsx
+++ to-travel/src/pages/About.jsx
@@ -1,9 +1,21 @@
 import {Link} from "react-router-dom"
+import aboutimg from "../assets/images/About.webp"
+import GitIcon from "../assets/images/GitIconFooter.png"
+import InIcon from "../assets/images/InIcon.png"
 
 export default function About() {
     return (
         <div id="aboutPage">
-            <h1 id="title">About Page</h1>
+            <h1 id="title">To Travel List App</h1>
+            <p>To Travel List App is a React app. It functions as a travel planner, allowing users to manage past and upcoming trips. Users can add, edit, delete, and view details of each trip easily. <br/> 
+              The intuitive interface makes trip organization seamless and efficient. Perfect for keeping track of all your travel adventures!
+            </p>
+            <img src={aboutimg} alt={aboutimg} />
+            <h2>Kilian D. Hernández Chirino</h2>
+            <div id="icons">
+                <a target="_blank" href="https://github.com/Kilianhc"><img id="gitIcon" src={GitIcon} alt={GitIcon}></img></a>
+                <a target="_blank" href="https://www.linkedin.com/in/kilian-hern%C3%A1ndez-chirino-b84aa8168/"><img id="inIcon" src={InIcon} alt={InIcon}></img></a>
+            </div>
         </div>
     )
 }
