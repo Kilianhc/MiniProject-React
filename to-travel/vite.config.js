83b7325ee8c32b79c0a0b7f7321be67556b81949
Kilian H <kilianhc@hotmail.com>
2025-04-11 19:20:20 +0100

    base root

--- to-travel/vite.config.js
+++ to-travel/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MiniProject-React/',
+  base: '/MiniProject-React/to-travel/',
   plugins: [react()],
 })
